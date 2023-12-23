66021e8a1612a607683334eeb6764958f9f5058e
Felipe Rosene Klapsztein <feliperosenek@gmail.com>
2023-12-23 14:48:31 -0300

    Update cnpjApi.js

--- cnpjApi.js
+++ cnpjApi.js
@@ -11,6 +11,8 @@ const {
 const minimist = require('minimist');
 const { addConsoleHandler } = require('selenium-webdriver/lib/logging');
 const params = minimist(process.argv.slice(2))
+const configBots = require('./bots-config.js');
+
 
 const sequelize = new Sequelize("eduard72_" + params.bd + "", "eduard72_wp625", "37@S0DSm(p", {
   host: 'sh-pro20.hostgator.com.br',
@@ -22,8 +24,6 @@ const sequelize = new Sequelize("eduard72_" + params.bd + "", "eduard72_wp625", 
   logging: false
 });
 
-console.log(" - " + params.acao + " - ");
-
 const delay = ms => new Promise(resolve => setTimeout(resolve, ms))
 var page = 0;
 var pages = 1;
@@ -31,7 +31,7 @@ var s = 0;
 var ordemBusca = "";
 
 
-sequelize.authenticate().then(() => {})
+sequelize.authenticate().then(() => { })
   .catch(err => {
     console.error('Erro ao conectar a banco de dados: ', err);
   });
@@ -39,70 +39,10 @@ sequelize.authenticate().then(() => {})
 async function getCNPJ() {
   try {
 
-    // --> Pega os CNAEs no banco
-   if (params.bot == 1) {
-    ordemBusca = '&& id > 1 && id <= 32';
-}
-if (params.bot == 2) {
-    ordemBusca = '&& id > 33 && id <= 64';
-}
-if (params.bot == 3) {
-    ordemBusca = '&& id > 65 && id <= 96';
-}
-if (params.bot == 4) {
-    ordemBusca = '&& id > 97 && id <= 128';
-}
-if (params.bot == 5) {
-    ordemBusca = '&& id > 129 && id <= 160';
-}
-if (params.bot == 6) {
-    ordemBusca = '&& id > 161 && id <= 192';
-}
-if (params.bot == 7) {
-    ordemBusca = '&& id > 193 && id <= 224';
-}
-if (params.bot == 8) {
-    ordemBusca = '&& id > 225 && id <= 256';
-}
-if (params.bot == 9) {
-    ordemBusca = '&& id > 257 && id <= 288';
-}
-if (params.bot == 10) {
-    ordemBusca = '&& id > 289 && id <= 320';
-}
-if (params.bot == 11) {
-    ordemBusca = '&& id > 321 && id <= 352';
-}
-if (params.bot == 12) {
-    ordemBusca = '&& id > 353 && id <= 384';
-}
-if (params.bot == 13) {
-    ordemBusca = '&& id > 385 && id <= 416';
-}
-if (params.bot == 14) {
-    ordemBusca = '&& id > 417 && id <= 448';
-}
-if (params.bot == 15) {
-    ordemBusca = '&& id > 449 && id <= 480';
-}
-if (params.bot == 16) {
-    ordemBusca = '&& id > 481 && id <= 512';
-}
-if (params.bot == 17) {
-    ordemBusca = '&& id > 513 && id <= 544';
-}
-if (params.bot == 18) {
-    ordemBusca = '&& id > 545 && id <= 576';
-}
-if (params.bot == 19) {
-    ordemBusca = '&& id > 577 && id <= 608';
-}
-if (params.bot == 20) {
-    ordemBusca = '&& id > 609 && id <= 642';
-}
+    const bot = params.bot; // Suponha que você tenha params.bot definido
+    const ordemBusca = configBots(bot);
 
-
-    var cnaes = await sequelize.query("SELECT cnae FROM `cnaes` WHERE statusBot" + params.bot + "=0 "+ ordemBusca +" ORDER BY RAND()", {
+    var cnaes = await sequelize.query("SELECT cnae FROM `cnaes` WHERE statusBot" + params.bot + "=0" + ordemBusca + " ORDER BY RAND()", {
       type: QueryTypes.SELECT
     });
 
@@ -111,35 +51,36 @@ if (params.bot == 20) {
       cnaesSearch = cnaes[i].cnae;
       // pesquisa por cnae
       const data = await consultarCNPJ.pesquisa({
-          atividade_principal_id: cnaesSearch,
-          estado_id: params.uf
-        },
+        atividade_principal_id: cnaesSearch,
+        estado_id: params.uf
+      },
         token,
         1
       );
 
-      console.log(" CNAE: " + cnaesSearch + "- total: "+ data.paginacao.total + "  |" + (i + 1) + "º de " + cnaes.length + " | Páginas: " + data.paginacao.paginas)
+      console.log(" CNAE: " + cnaesSearch + "- total: " + data.paginacao.total + "  |" + (i + 1) + "º de " + cnaes.length + " | Páginas: " + data.paginacao.paginas)
       console.log(" - - " + params.acao + " - - ");
 
       var pageRound = params.paginas
 
-      if( data.paginacao.paginas < pageRound){
-        pageRound =  data.paginacao.paginas
+      if (data.paginacao.paginas < pageRound) {
+        pageRound = data.paginacao.paginas
       }
 
       for (var t = 0; t < pageRound; t++) {
         // --> Define uma página aleatória
-        if(pageRound > params.paginas){
+        if (pageRound > params.paginas) {
           page = Math.floor(Math.random() * pageRound) + 1;
-        }else{page = t+1}
-        
-        console.log("Página: " + page +"  | "+ (i + 1) + "º de " + cnaes.length);
+        } else { page = t + 1 }
+
+        console.log("Página: " + page + "  | " + (i + 1) + "º de " + cnaes.length);
 
         // --> Pesquisa por página
+        
         const data2 = await consultarCNPJ.pesquisa({ // --> Consulta o CNAE
-            atividade_principal_id: cnaesSearch,
-            estado_id: params.uf
-          },
+          atividade_principal_id: cnaesSearch,
+          estado_id: params.uf
+        },
           token,
           page
         );
@@ -168,10 +109,10 @@ if (params.bot == 20) {
           telefone2 = industria.estabelecimento.telefone2;
           cnae = industria.estabelecimento.atividade_principal.id;
           cadastro = industria.estabelecimento.situacao_cadastral;
-          if(industria.porte){
-          porte = industria.porte.descricao;
-          }else{
-          porte = ''
+          if (industria.porte) {
+            porte = industria.porte.descricao;
+          } else {
+            porte = ''
           }
           produto_2 = industria.estabelecimento.atividade_principal.descricao;
           produto_3 = industria.estabelecimento.atividades_secundarias;
@@ -285,7 +226,8 @@ if (params.bot == 20) {
           if (matriz == null) {
             matriz = ""
           }
-          if (filial == null) {filial = ""
+          if (filial == null) {
+            filial = ""
           }
 
           var res = 0;
@@ -296,11 +238,15 @@ if (params.bot == 20) {
           });
 
           // --> Filtros para inserir no banco de dados
-          if (verificaDuplicata != "" || nome.includes("'") || endereco.includes("'") || bairro.includes("'") || municipio.includes("'") || telefone == "" ||  email == "" || fantasia.includes("'") || tipo_logradouro.includes("'") || cadastro.includes("Baixada") || cadastro.includes("Inapta")) {
+          if (verificaDuplicata != "" || nome.includes("'") || endereco.includes("'") || bairro.includes("'") || municipio.includes("'") || telefone == "" || email == "" || fantasia.includes("'") || tipo_logradouro.includes("'") || cadastro.includes("Baixada") || cadastro.includes("Inapta")) {
 
           } else {
-            await sequelize.query("INSERT INTO catalogo (nome,cnpj, fantasia, endereco, tipo_logradouro, numero,complemento, bairro, cep, municipio, uf,pais,ddd_telefone, telefone, email, ddd_telefone2, telefone2, capital, cnae, produto_1, produto_2, produto_3, matriz, filial,ano_fundacao,produtos,materias_primas,nro_funcionarios,importa,exporta) VALUES ('" + nome + "','" + cnpj + "','" + fantasia + "','" + endereco + "','" + tipo_logradouro + "','" + numero + "','" + complemento + "','" + bairro + "','" + cep + "','" + municipio + "','" + uf + "','" + pais + "','" + ddd_telefone + "','" + telefone + "','" + email + "','" + ddd_telefone2 + "','" + telefone2 + "','" + capital + "','" + cnae + "','" + produto_1 + "','" + produto_2 + "','" + produto_3 + "','" + matriz + "','" + filial + "','" + fundacao + "',0,0,0,0,0 ) ")
-            res = 1;
+            try{
+              await sequelize.query("INSERT INTO catalogo (nome,cnpj, fantasia, endereco, tipo_logradouro, numero,complemento, bairro, cep, municipio, uf,pais,ddd_telefone, telefone, email, ddd_telefone2, telefone2, capital, cnae, produto_1, produto_2, produto_3, matriz, filial,ano_fundacao,produtos,materias_primas,nro_funcionarios,importa,exporta) VALUES ('" + nome + "','" + cnpj + "','" + fantasia + "','" + endereco + "','" + tipo_logradouro + "','" + numero + "','" + complemento + "','" + bairro + "','" + cep + "','" + municipio + "','" + uf + "','" + pais + "','" + ddd_telefone + "','" + telefone + "','" + email + "','" + ddd_telefone2 + "','" + telefone2 + "','" + capital + "','" + cnae + "','" + produto_1 + "','" + produto_2 + "','" + produto_3 + "','" + matriz + "','" + filial + "','" + fundacao + "',0,0,0,0,0 ) ")
+              res = 1;
+            }catch{
+              console.log("ERRO MYSQL <-------------")
+            }
           }
           if (res == 0) {
             res = ""
@@ -315,7 +261,7 @@ if (params.bot == 20) {
       }
       // --> Informa que o cnae já foi usado
       await sequelize.query("UPDATE cnaes SET statusBot" + params.bot + "=1 WHERE cnae=" + cnaesSearch + "")
-      await sequelize.query("UPDATE cnaes SET total="+ data.paginacao.total +" WHERE cnae=" + cnaesSearch + "")
+      await sequelize.query("UPDATE cnaes SET total=" + data.paginacao.total + " WHERE cnae=" + cnaesSearch + "")
     }
 
   } catch (error) {
